3370e76148f1987dc64bfb59d22166b38155815f
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-07-26 13:44:53 +0300

    fix contactform

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import css from './ContactForm.module.css';
-import { Formik, Form, Field, ErrorMessage } from 'Formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { nanoid } from 'nanoid';
 import { useId } from 'react';
 import * as Yup from 'yup';
